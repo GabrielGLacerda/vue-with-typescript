897d0ec9855e84b05fdd752802656ed70eebbf14
Gabriel Gomes <gabrielglacerda@hotmail.com>
2022-05-24 10:44:22 -0300

    correções de fonte

--- hyrule-jobs/src/App.vue
+++ hyrule-jobs/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
+import { ref } from 'vue'
+  const name = ref('Link')
+  const age = ref(30)
+  const weapon = ref('sword')
 </script>
 
 <template>
   <div class="app">
-    hello vue3 + ts
+    {{name}} - {{age}} - {{weapon}}
   </div>
 </template>
 
